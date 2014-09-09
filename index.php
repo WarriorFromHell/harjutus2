c27fdb8505e21481d63edba918d0301e997c3c14
Markus <markus.joemets@khk.ee>
2014-09-09 14:15:29 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;
+charset=utf-8">
+<title> harjutus-2 - PHP põhitõed </title>
+</head>
+
+<body>
+<h1> See dokument on minu esimene katsetus PhP-ga </h1>
+<?php
+	echo "Tere" . "siin veel midagi";
+?>
+<br>
+<h2> test test </h2>
+<!-- siin on kommentaar-->
+<p> erinevad tehted </p>
+
+</body>
+</html>
